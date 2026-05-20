f108b6577748b590e84faf9a0f69aff32b61eeb5
Leoner Laboy <LeonerLaboy@gmail.com>
2026-05-20 18:04:43 -0400

    Move image on window

---
+++ move_image.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+def move_up(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()-10)
+
+def move_down(event):
+    label.place(x=label.winfo_x(), y=label.winfo_y()+10)
+
+def move_left(event):
+    label.place(x=label.winfo_x() - 10, y=label.winfo_y())
+
+def move_right(event):
+    label.place(x=label.winfo_x() + 10, y=label.winfo_y())
+
+window = Tk()
+window.geometry('500x500')
+
+window.bind('<w>', move_up)
+window.bind('<s>', move_down)
+window.bind('<a>', move_left)
+window.bind('<d>', move_right)
+
+my_image = PhotoImage(file='pizza.png')
+label = Label(window, image=my_image)
+label.place(x=0,y=0)
+
+window.mainloop()
